d115b24118484c3fd336d23733afbe901097f808
danteata <dan.abakah@gmail.com>
2011-07-12 15:28:29 +0000

    deleted the default index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApplication::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
-  get "pages/contact"
+  get "/pages/home"
+  match "/home", :to=>"pages#home"
+  match "/help" =>"pages#help"
+  match "/contact" =>"pages#contact"
+  match "/about" =>"pages#about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +54,7 @@ SampleApplication::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
