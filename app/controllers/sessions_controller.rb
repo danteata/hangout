c85c3d992fbece2b4c5779280a77d7f47deb2d32
danteata <dan.abakah@gmail.com>
2011-07-21 17:40:53 +0000

    sign in at 42 tests

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  def new
+    @title = "Sign in"
+  end
+
+  def create
+    
+  end
+
+  def destroy
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApplication::Application.routes.draw do
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
+
   match "/signup", :to=> "users#new"
+  match "/signin", :to => "sessions#new"
+  match "/signout", :to => "sessions#destroy"
   match "/home", :to=>"pages#home"
   match "/help" =>"pages#help"
   match "/contact" =>"pages#contact"
