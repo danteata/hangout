cd392edd4d04897fc519448c691ceac174235c44
danteata <dan.abakah@gmail.com>
2011-08-05 00:54:58 +0000

    done with modelling friendship

---
+++ db/migrate/20110805001310_create_friendships.rb
@@ -0,0 +1,17 @@
+class CreateFriendships < ActiveRecord::Migration
+  def self.up
+    create_table :friendships do |t|
+      t.integer :user_id
+      t.integer :friend_id
+      t.string :status
+      t.datetime :created_at
+      t.datetime :accepted_at
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :friendships
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110804190455) do
+ActiveRecord::Schema.define(:version => 20110805001310) do
 
   create_table "faqs", :force => true do |t|
     t.integer  "user_id"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(:version => 20110804190455) do
     t.text     "television"
     t.text     "books"
     t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friendships", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "accepted_at"
     t.datetime "updated_at"
   end
 
