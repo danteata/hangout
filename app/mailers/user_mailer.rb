e5bae95c3779e0806210d7b04a4da9431de7736a
danteata <dan.abakah@gmail.com>
2011-08-15 13:33:50 +0000

    edited the mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,18 +9,25 @@ class UserMailer < ActionMailer::Base
   end
 
   def reminder(user)
+    #generates a new password and hashes it into database while making it available in mail as @password
     @user = user
+    @password = @user.password = generated_password 
+    @user.save!
+
     mail(:to =>user.email,
          :subject=>"Your password reminder.")
   end
 
   def friend_request(user, friend)
+
     @user = user
     @friend = friend
     @accept_url = url_for(:controller => "friendships", :action =>"accept",  :id=>@user.id, :host =>"localhost:3000")
     @decline_url = url_for(:controller =>"friendships",:action => "decline", :id => @user.id, :host =>"localhost:3000")
+
     mail(:to=>@friend.email,
         :subject=>"Request for your friendship")
+
   end
 
   #method to render the corresponding message
@@ -33,4 +40,11 @@ class UserMailer < ActionMailer::Base
     mail(:to =>recipient.email, :subject => message.subject)
     #mail(:to =>"dantheta@yahoo.co.uk", :subject => "just testing it out")
   end
+
+  private 
+
+    def generated_password(size= 8)
+      chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o O l 1 0) #removed characters that can confuse the user 
+      (1..size).collect{|a| chars[rand(chars.size)]}.join
+    end
 end
